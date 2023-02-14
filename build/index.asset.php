fae009259c2539dff7fd7001fbe8c78e02ecd0ae
Jen Wachter <jwachter@jhu.edu>
2023-02-14 16:41:29 -0500

    recompiled lib

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('fetch-jsonp', 'lodash', 'react', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '5eac3294e583f681ead7');
+<?php return array('dependencies' => array('fetch-jsonp', 'lodash', 'react', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'eec7a8288de04c2dcbe7');
