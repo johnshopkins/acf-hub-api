5e0315cb0bcd32d10ea2cc595abab1a34cb85920
Jen Wachter <jwachter@jhu.edu>
2025-02-14 13:21:10 -0500

    Built assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('fetch-jsonp', 'lodash', 'react', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'bc9dfd6a3118e65caf1a');
+<?php return array('dependencies' => array('fetch-jsonp', 'lodash', 'react', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'a4666dc08b8f56c89109');
